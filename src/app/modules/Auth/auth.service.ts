66fa09895cdbeb65470f85fd7a511d63cb54ddd5
Monirul Hassan <hassan.monirul@gmail.com>
2024-12-28 23:44:40 +0600

    lint corrected

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -199,7 +199,7 @@ const forgetPassword = async (userId: string) => {
 
   sendEmail(user.email, resetUILink);
 
-  console.log(resetUILink);
+  
 };
 
 const resetPassword = async (
@@ -234,7 +234,7 @@ const resetPassword = async (
   //localhost:3000?id=A-0001&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJBLTAwMDEiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE3MDI4NTA2MTcsImV4cCI6MTcwMjg1MTIxN30.-T90nRaz8-KouKki1DkCSMAbsHyb9yDi0djZU3D6QO4
 
   if (payload.id !== decoded.userId) {
-    console.log(payload.id, decoded.userId);
+    
     throw new AppError(StatusCodes.FORBIDDEN, 'You are forbidden!');
   }
 
